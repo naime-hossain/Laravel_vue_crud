363f606204ca940f6f327f82bc34592fc7653db8
Naime <hossain.naime@yahoo.com>
2017-11-14 13:19:43 +0600

    vue side completed but showing error

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -8,9 +8,11 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Companies</div>
  
-                    <div class="panel-body">
-                        Coming soon...
-                    </div>
+                    
+                       <div class="panel-body table-responsive">
+                            <router-view name="companiesIndex"></router-view>
+                            <router-view></router-view>
+                      </div>
                 </div>
             </div>
         </div>
